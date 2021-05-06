ee1cc22941f30a1f6c54156b22aed72ecdcfd970
林宛慧 <aa0960757329@gmail.com>
2021-05-06 12:03:49 +0800

    0506_套版新北市政府；並修改首頁影片播放模式。

---
+++ taipei/assets/js/timeline.js
@@ -0,0 +1,504 @@
+let data = [
+    {
+        id: '1',
+        year: '2015',
+        next: 'year__inner2',
+        prev: 'year__inner8',
+        photo: [
+            {
+                id: '1',
+                src: './assets/images/timeline/2015_1.jpg',
+                word: '鄰里交通環境改善計畫自2015年8月起，2020年全市施作完成',
+
+            },
+            {
+                id: '2',
+                src: './assets/images/timeline/2015_2.jpg',
+                word: '田園城市的臺北，增加綠地面積佔比、高層立體綠化，累積26萬餘市民參與',
+
+            },
+            {
+                id: '3',
+                src: './assets/images/timeline/2015_3.jpg',
+                word: '下一個25年的起點，以北門為中心的「西區門戶計畫」，將臺北西區與桃園中正國際機場串連，成為嶄新的國際城市',
+
+            }
+        ]
+    },
+    {
+        id: '2',
+        year: '2016',
+        next: 'year__inner3',
+        prev: 'year__inner1',
+        photo: [
+            {
+                id: '1',
+                src: './assets/images/timeline/2016_1.jpg',
+                word: '2016年加入源於法國巴黎的白晝之夜，全球跨夜藝術慶典在台北發生，迄今參與總人數達182萬人',
+
+            },
+            {
+                id: '2',
+                src: './assets/images/timeline/2016_2.jpg',
+                word: '找回台北舊城區快被遺忘的人文、歷史與光榮，就從北門重現開始',
+            },
+            {
+                id: '3',
+                src: './assets/images/timeline/2016_3.jpg',
+                word: '實踐共融價值，讓孩子有同等權利在公園玩樂，是市府推動「共融式遊具」初衷',
+            }
+        ]
+    },
+    {
+        id: '3',
+        year: '2017',
+        next: 'year__inner4',
+        prev: 'year__inner2',
+        photo: [
+            {
+                id: '1',
+                src: './assets/images/timeline/2017_1.jpg',
+                word: '「臺灣走出去，世界走進來，讓世界看見台灣」，臺北世大運凝聚臺灣人民的心',
+
+            },
+            {
+                id: '2',
+                src: './assets/images/timeline/2017_2.jpg',
+                word: '2017-2019年連續三年拿下招商王，公開透明、合理健全的招商制度，6年民間總投資金額達1,866億元',
+
+            }
+            ,
+            {
+                id: '3',
+                src: './assets/images/timeline/2017_3.jpg',
+                word: '社區整合照顧服務（石頭湯計畫），客製且即時服務每位長者的需求 ，由北市府搭起平臺，讓更多資源投入',
+
+            }
+        ]
+    },
+    {
+        id: '4',
+        year: '2018',
+        next: 'year__inner5',
+        prev: 'year__inner3',
+        photo: [
+            {
+                id: '1',
+                src: './assets/images/timeline/2018_1.jpg',
+                word: '開齋節是台北多元開放的象徵之一，萬名穆斯林齊聚讓台北成為共融城市',
+
+            },
+            {
+                id: '2',
+                src: './assets/images/timeline/2018_2.jpg',
+                word: '精實管理，堅持財政紀律，6年共還債570億，台北人均負債從5.4萬降至3.4萬',
+
+            },
+            {
+                id: '3',
+                src: './assets/images/timeline/2018_3.jpg',
+                word: '全臺灣第一例成功的公辦都更，延宕20多年的斯文里整宅都更計畫，在2018年重新開工，是公辦都更制度化的開始',
+
+            }
+        ]
+    },
+    {
+        id: '5',
+        year: '2019',
+        next: 'year__inner6',
+        prev: 'year__inner4',
+        photo: [
+            {
+                id: '1',
+                src: './assets/images/timeline/2019_1.jpg',
+                word: '臺北市嬰兒公托及準公共化達75%，透過多元托育服務，舒緩父母經濟壓力',
+
+            },
+            {
+                id: '2',
+                src: './assets/images/timeline/2019_2.jpg',
+                word: '市場是城市生活的縮影、是我們的日常，讓市場成為一個觀光及體驗新服務的所在',
+
+            },
+            {
+                id: '3',
+                src: './assets/images/timeline/2019_3.jpg',
+                word: '不受設站限制的YouBike2.0，將於2022年擴大設置至1200站',
+
+            }
+        ]
+    },
+    {
+        id: '6',
+        year: '2020',
+        next: 'year__inner7',
+        prev: 'year__inner5',
+        photo: [
+            {
+                id: '1',
+                src: './assets/images/timeline/2020_1.jpg',
+                word: '偉大的城市需要世代共同努力，百年臺北的市政基業，傳承接力一棒接一棒',
+            },
+            {
+                id: '2',
+                src: './assets/images/timeline/2020_2.jpg',
+                word: '北市23個新創基地，鼓勵將臺北市成為新創育成的試驗場域',
+            },
+            {
+                id: '3',
+                src: './assets/images/timeline/2020_3.jpg',
+                word: '臺北酷課雲與13縣市簽署MOU、與僑委會合作讓1,054所僑校參與，並獲2020年第六屆IDC亞太區智慧城市大獎教育類別最佳智慧城市獎項',
+            }
+        ]
+    },
+    {
+        id: '7',
+        year: '2021',
+        next: 'year__inner8',
+        prev: 'year__inner6',
+        photo: [
+            {
+                id: '1',
+                src: './assets/images/timeline/2021_1.jpg',
+                word: '臺北大縱走，全長92公里7段山徑，140公里盡覽山與水之美的嶄新旅途',
+            },
+            {
+                id: '2',
+                src: './assets/images/timeline/2021_2.jpg',
+                word: '實現居住正義，打造130處、19,661戶社宅，讓市民享有更多居住選擇',
+            },
+            {
+                id: '3',
+                src: './assets/images/timeline/2021_3.jpg',
+                word: '打造5+3無圍牆博物館，讓城市的每一角落都有各自的「故事」',
+            }
+        ]
+    },
+    {
+        id: '8',
+        year: '2022',
+        next: 'year__inner1',
+        prev: 'year__inner7',
+        photo: [
+            {
+                id: '1',
+                src: './assets/images/timeline/2022_1.jpg',
+                word: '臺北馬拉松取得2019年銅標籤認證、2022年將努力取得金標籤認證',
+            },
+            {
+                id: '2',
+                src: './assets/images/timeline/2022_2.jpg',
+                word: '臺北表演藝術中心整合生態產業鏈，培育優秀人才，健全表演藝術的產業環境',
+            },
+            {
+                id: '3',
+                src: './assets/images/timeline/2022_3.jpg',
+                word: '北市透過大眾運輸導向型發展（Transit-oriented development，TOD），建構完善捷運路網，帶動地區經濟發展',
+            }
+        ]
+    },
+]
+
+
+window.setInterval(function () {
+    // if ($('.ani_chart1').attr('src') == './assets/images/chart/chart1-teacher_1.png') {
+    //     $('.ani_chart1').attr('src', './assets/images/chart/chart1-teacher_2.png')
+    // } else {
+    //     $('.ani_chart1').attr('src', './assets/images/chart/chart1-teacher_1.png')
+    // }
+
+    if ($('.ani_chart2').attr('src') == './assets/images/chart/chart1-student_1.png') {
+        $('.ani_chart2').attr('src', './assets/images/chart/chart1-student_2.png')
+    } else {
+        $('.ani_chart2').attr('src', './assets/images/chart/chart1-student_1.png')
+    }
+
+    if ($('.ani_chart3').attr('src') == './assets/images/chart/chart2-man_1.png') {
+        $('.ani_chart3').attr('src', './assets/images/chart/chart2-man_2.png')
+    } else if ($('.ani_chart3').attr('src') == './assets/images/chart/chart2-man_2.png') {
+        $('.ani_chart3').attr('src', './assets/images/chart/chart2-man_3.png')
+    } else if ($('.ani_chart3').attr('src') == './assets/images/chart/chart2-man_3.png') {
+        $('.ani_chart3').attr('src', './assets/images/chart/chart2-man_1.png')
+    }
+
+    if ($('.ani_chart4').attr('src') == './assets/images/chart/chart2-family_1.png') {
+        $('.ani_chart4').attr('src', './assets/images/chart/chart2-family_2.png')
+    } else if ($('.ani_chart4').attr('src') == './assets/images/chart/chart2-family_2.png') {
+        $('.ani_chart4').attr('src', './assets/images/chart/chart2-family_3.png')
+    } else if ($('.ani_chart4').attr('src') == './assets/images/chart/chart2-family_3.png') {
+        $('.ani_chart4').attr('src', './assets/images/chart/chart2-family_1.png')
+    }
+
+    if ($('.ani_chart5').attr('src') == './assets/images/chart/chart3-bus_1.png') {
+        $('.ani_chart5').attr('src', './assets/images/chart/chart3-bus_2.png')
+    } else if ($('.ani_chart5').attr('src') == './assets/images/chart/chart3-bus_2.png') {
+        $('.ani_chart5').attr('src', './assets/images/chart/chart3-bus_3.png')
+    } else if ($('.ani_chart5').attr('src') == './assets/images/chart/chart3-bus_3.png') {
+        $('.ani_chart5').attr('src', './assets/images/chart/chart3-bus_1.png')
+    }
+
+    if ($('.ani_chart6').attr('src') == './assets/images/chart/chart3-family_1.png') {
+        $('.ani_chart6').attr('src', './assets/images/chart/chart3-family_2.png')
+    } else if ($('.ani_chart6').attr('src') == './assets/images/chart/chart3-family_2.png') {
+        $('.ani_chart6').attr('src', './assets/images/chart/chart3-family_1.png')
+    }
+
+}, 350)
+
+var prev = 'year__inner8'
+var next = 'year__inner2'
+
+$('.section3__con--down .prev').click(function () {
+    $('.' + prev).click()
+})
+
+$('.section3__con--down .next').click(function () {
+    $('.' + next).click()
+})
+
+let year__inner = document.querySelectorAll('.year__inner')
+for (let i = 0; i < year__inner.length; i++) {
+    year__inner[i].addEventListener('click', function () {
+        let num = this.getAttribute('data-num')
+        $('.section3__con--down .section3__inner--left span').html(data[num - 1].year)
+        $('.section3__con--down .section3__inner--left .bg').html(data[num - 1].year)
+
+        prev = data[num - 1].prev
+        next = data[num - 1].next
+
+        var owl_1 = $(".owl-carousel")
+        owl_1.trigger('destroy.owl.carousel');
+        $(".owl-carousel").html('')
+        for (let j = 0; j < data[num - 1].photo.length; j++) {
+            owl = document.querySelector('.owl-carousel')
+            let div = document.createElement('div')
+            div.setAttribute('class', 'item')
+            let img = document.createElement('img')
+            // img.setAttribute('class', 'owl-lazy')
+            img.setAttribute('src', data[num - 1].photo[j].src)
+            let p = document.createElement('p')
+            p.innerHTML = data[num - 1].photo[j].word
+            div.append(img)
+            div.append(p)
+            owl.append(div)
+        }
+
+        $('html,body').animate(
+            { scrollTop: $('.section3__con--down').offset().top },
+            150
+        )
+
+        owl_1.owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 100,
+            center: true,
+            autoplay: false,
+            autoplayTimeout: 4000,
+            autoplayHoverPause: false,
+            dots: true,
+            nav: true,
+            mergeFit: true,
+            fluidSpeed: 600,
+
+            // lazyLoad: true,
+            smartSpeed: 600,
+            responsive: {
+                0: {
+                    items: 1,
+                    touchDrag: true,
+                    mouseDrag: true,
+                },
+                768: {
+                    items: 2,
+                    touchDrag: true,
+                    mouseDrag: true,
+                },
+            }
+        });
+    })
+}
+
+(function ($) {
+    $.fn.animateNumbers = function (stop, commas, duration, ease) {
+        return this.each(function () {
+            var $this = $(this);
+            var start = parseInt($this.text().replace(/,/g, ""));
+            commas = (commas === undefined) ? true : commas;
+            $({ value: start }).animate({ value: stop }, {
+                duration: duration == undefined ? 1000 : duration,
+                easing: ease == undefined ? "swing" : ease,
+                step: function () {
+                    $this.text(Math.floor(this.value));
+                    if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                },
+                complete: function () {
+                    if (parseInt($this.text()) !== stop) {
+                        $this.text(stop);
+                        if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                    }
+                }
+            });
+        });
+    };
+})(jQuery);
+
+var check1 = true,
+    check2 = true,
+    check3 = true;
+
+$(window).on('scroll', function () {
+    var wScroll = $(this).scrollTop();
+    if ($(this).scrollTop() > 100) {
+        $(".goTop").fadeIn()
+    } else {
+        $(".goTop").fadeOut()
+    }
+
+    var chart1 = $('.section4__inner--chart1').offset().top,
+        chart2 = $('.section4__inner--chart2').offset().top,
+        chart3 = $('.section4__inner--chart3').offset().top;
+
+    windowScroll = $(this).scrollTop();
+    if (chart1 - document.body.clientHeight / 2 < windowScroll) {
+
+        if (check1) {
+            $("#chart1").animateNumbers(1093680, true, 3000);
+            $("#chart2").animateNumbers(10925, true, 3000);
+            $("#chart3").animateNumbers(400, true, 2000);
+            $("#chart4").animateNumbers(36046, true, 3000);
+            // $(".chart__inner--line1").addClass('chart__inner--line1Ani');
+            $(".chart__inner--line2").addClass('chart__inner--line2Ani');
+            $(".chart__inner--line3").addClass('chart__inner--line3Ani');
+            $(".chart__inner--line4").addClass('chart__inner--line4Ani');
+            check1 = false;
+        }
+    }
+
+    if (chart2 - document.body.clientHeight / 2 < windowScroll) {
+        if (check2) {
+            $("#chart5").animateNumbers(52307, true, 3000);
+            $("#chart6").animateNumbers(715433, true, 3000);
+            check2 = false;
+        }
+    }
+
+    if (chart3 - document.body.clientHeight / 2 < windowScroll) {
+        if (check3) {
+            $("#chart7").animateNumbers(32, true, 2000);
+            $("#chart8").animateNumbers(200000, true, 3000);
+            $("#chart9").animateNumbers(154156, true, 3000);
+            $("#chart10").animateNumbers(43600, true, 3000);
+            $("#chart11").animateNumbers(1350, true, 3000);
+            $("#chart12").animateNumbers(13611560, true, 3000);
+            $("#chart13").animateNumbers(13118800, true, 3000);
+            check3 = false;
+        }
+    }
+});
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: 'W4JKvYgCu5s',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    player.mute()
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 0);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+    player.mute()
+}
+
+$(".title_kv--inner3").click(function () {
+    $(".popup_video").css("display","block");
+    $("body").css("overflow","hidden");
+    player.playVideo();
+    window.setTimeout(function () {
+        document.querySelector('#player').style.zIndex = '100'
+        // document.querySelector('.video-container').style.backgroundImage = 'url("")'
+    }, 200)
+    // $('.yt').attr('src', 'https://www.youtube.com/embed/AtlS7dAG_W8?muted=1&autoplay=1')
+})
+
+$(".icon--close").click(function () {
+    $(".popup_video").css("display","none");
+    $("body").css("overflow","auto");
+    player.stopVideo();
+    player.mute()
+})
+
+$(".goTop").click(function () {
+    $('html,body').animate(
+        { scrollTop: $('.section1').offset().top },
+        300
+    )
+})
+
+$(window).on("load", function () {
+    $(".owl-carousel-1").owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 100,
+        center: true,
+        autoplay: false,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: false,
+        dots: true,
+        nav: true,
+        mergeFit: true,
+        // lazyLoad: true,
+        fluidSpeed: 600,
+        smartSpeed: 600,
+        responsive: {
+            0: {
+                items: 1,
+                touchDrag: true,
+                mouseDrag: true,
+            },
+            768: {
+                items: 2,
+                touchDrag: true,
+                mouseDrag: true,
+            },
+        }
+    });
+})
+
+window.setTimeout(function () {
+    if (window.innerWidth > 768) {
+        var section3DownH = $('.section3__con--down').height()
+        $('.section3__con--down').css('min-height', section3DownH)
+        var section3DownH = $('.section3__con--down').height()
+        $('.section3__con--down .section3__inner--left').css('min-height', section3DownH)
+        $('.section3__con--down .section3__inner--right').css('min-height', section3DownH)
+    }
+}, 3000)
